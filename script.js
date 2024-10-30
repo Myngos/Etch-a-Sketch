966c235843455752026a97c571dfd1c45f393494
Mingos <137617609+Myngos@users.noreply.github.com>
2024-10-30 19:44:44 +0200

    random colors for the grids

--- script.js
+++ script.js
@@ -6,13 +6,17 @@ for(let i = 1; i <= 256; i++){
     divs.textContent = i;
     divs.style.cssText = "border:3px Solid Blue; width: 50px; height: 50px; display:flex; justify-content:center;align-items: center;";
 
-    divs.addEventListener('mouseover', ()=>{
-        divs.style.backgroundColor = "blue";
-    })
+    divs.addEventListener("mouseover", function() {
+        divs.style.backgroundColor = `rgb(${randomColorValue()}, ${randomColorValue()}, ${randomColorValue()})`;
+    });
 
     divs.addEventListener('mouseout', ()=>{
         divs.style.backgroundColor = ""
     })
 
     container.appendChild(divs);
+}
+
+function randomColorValue() {
+    return Math.floor(Math.random() * 256);
 }
