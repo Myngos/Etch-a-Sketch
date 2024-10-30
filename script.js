9b4b1782da659f964eb7e1bbdca7dfd04ae2d8a7
Mingos <137617609+Myngos@users.noreply.github.com>
2024-10-30 18:07:47 +0200

    first JavaScript div

---
+++ script.js
@@ -0,0 +1,6 @@
+const div = document.querySelector(".container");
+
+const divs = document.createElement('div');
+divs.textContent = 1;
+divs.style.cssText = "border:3px Solid Blue; width: 50px; height: 50px;"
+div.appendChild(divs)
