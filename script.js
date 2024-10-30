58ed6fc91a9638b74bfab979930907df20dfc03e
Mingos <137617609+Myngos@users.noreply.github.com>
2024-10-30 18:26:36 +0200

    styles for the grids

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ const div = document.querySelector(".container");
 for(let i = 1; i <= 256; i++){
     const divs = document.createElement('div');
     divs.textContent = i;
-    divs.style.cssText = "border:3px Solid Blue; width: 50px; height: 50px; display:flex; justify-content:center;align-items: center; margin: 20px;";
-    div.appendChild(divs)
+    divs.style.cssText = "border:3px Solid Blue; width: 50px; height: 50px; display:flex; justify-content:center;align-items: center;";
+    div.appendChild(divs);
 }
