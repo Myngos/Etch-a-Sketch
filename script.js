bd967233f995ef7d91a806343d854f36c73aa35f
Mingos <137617609+Myngos@users.noreply.github.com>
2024-10-30 19:09:04 +0200

    grid hovers
    Added hover effects to the grids using javaScript mouseover and mouseout events

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-const div = document.querySelector(".container");
+const container = document.querySelector(".container");
 
 
 for(let i = 1; i <= 256; i++){
     const divs = document.createElement('div');
     divs.textContent = i;
     divs.style.cssText = "border:3px Solid Blue; width: 50px; height: 50px; display:flex; justify-content:center;align-items: center;";
-    div.appendChild(divs);
+
+    divs.addEventListener('mouseover', ()=>{
+        divs.style.backgroundColor = "blue";
+    })
+
+    divs.addEventListener('mouseout', ()=>{
+        divs.style.backgroundColor = ""
+    })
+
+    container.appendChild(divs);
 }
