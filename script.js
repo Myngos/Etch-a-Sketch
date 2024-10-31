c7cbacc8603a5e9e5d2a54e61f366ecb18661e51
Mingos <137617609+Myngos@users.noreply.github.com>
2024-11-01 00:56:29 +0200

    button fully done

--- script.js
+++ script.js
@@ -21,5 +21,27 @@ function randomColorValue() {
 }
 
 document.getElementById('btn').addEventListener('click', ()=>{
-    prompt('Enter the number of squares per side for the new grid!');
+
+    container.textContent = "";
+    
+    let i = prompt('Enter the number of squares per side for the new grid!');
+    
+    if(i > 100 || i < 1){
+        let i = prompt("Enter a number between 1 and 100!");
+    }
+
+    for(j = 1; j <= i; j++){
+        const divs = document.createElement('div');
+   
+    divs.style.cssText = "border:2px solid blue; width: 50px; height: 50px; display:flex; justify-content:center;align-items: center;";
+    divs.addEventListener("mouseover", function() {
+        divs.style.backgroundColor = `rgb(${randomColorValue()}, ${randomColorValue()}, ${randomColorValue()})`;
+    });
+
+  divs.addEventListener("mouseout", ()=>{
+    divs.style.backgroundColor = "";
+  }) 
+
+    container.appendChild(divs);
+    }
 })
